b3850a09a7eb1c62f8b830fa29aa1c5e38b8157b
vovchik33 <vovchik33@gmail.com>
2022-08-07 17:56:30 +0300

    Feature: Text Input handling

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, {useState} from "react";
 
 function App() {
   const [likes, setLikes] = useState(0);
+  const [value, setValue] = useState("Text");
   
   return (
     <div className="App">
       <h2>{likes}</h2>
+      <h2>{value}</h2>
+      <input value={value} onChange={(e) => setValue(e.target.value)}/>
       <button onClick={() => setLikes(likes-1)}>Decrease</button>
       <button onClick={() => setLikes(likes+1)}>Increase</button>
     </div>
