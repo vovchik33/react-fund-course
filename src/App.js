5d255fc72c99651c626b5c4ea3ae80126191251d
vovchik33 <vovchik33@gmail.com>
2022-08-07 18:42:47 +0300

    Feature: Post component is added

--- src/App.js
+++ src/App.js
@@ -2,16 +2,29 @@ import React, {useState} from "react";
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 
+import "./styles/App.css";
+import PostItem from "./components/PostItem";
+
 function App() {
   const [value, setValue] = useState("Text");
   
   return (
     <div className="App">
-      <Counter />
-      <ClassCounter />
-      
-      <h2>{value}</h2>
-      <input value={value} onChange={(e) => setValue(e.target.value)}/>
+      <PostItem post={{
+        id: 1,
+        title: "Post 1",
+        body: "Post text"
+      }}/>
+      <PostItem post={{
+        id: 2,
+        title: "Post 2",
+        body: "Post text"
+      }}/>
+      <PostItem post={{
+        id: 3,
+        title: "Post 3",
+        body: "Post text"
+      }}/>
     </div>
   );
 }
