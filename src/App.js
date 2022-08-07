0a2848b4994bc78778dc7200eca9ef4c8014a03c
vovchik33 <vovchik33@gmail.com>
2022-08-07 17:53:55 +0300

    Feature: Likes counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
+  const [likes, setLikes] = useState(0);
+  
   return (
     <div className="App">
-      Application
+      <h2>{likes}</h2>
+      <button onClick={() => setLikes(likes-1)}>Decrease</button>
+      <button onClick={() => setLikes(likes+1)}>Increase</button>
     </div>
   );
 }
